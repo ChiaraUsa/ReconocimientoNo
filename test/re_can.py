7f13921afcf897917dc0b9e0237e5b701e34e9a4
ChiaraUsa <chiara.valenzuela01@correo.usa.edu.co>
2024-06-08 13:55:09 -0500

    Cambios en el código de reconocimiento en canvas

--- test/re_can.py
+++ test/re_can.py
@@ -4,39 +4,35 @@ import mediapipe as mp
 from collections import deque
 from joblib import load
 import warnings
+
 warnings.filterwarnings("ignore", message="X does not have valid feature names")
 
-# Giving different arrays to handle colour points of different colour
-bpoints = [deque(maxlen=1024)]
-gpoints = [deque(maxlen=1024)]
-rpoints = [deque(maxlen=1024)]
-ypoints = [deque(maxlen=1024)]
-
-# These indexes will be used to mark the points in particular arrays of specific colour
-blue_index = 0
-green_index = 0
-red_index = 0
-yellow_index = 0
+# Giving different arrays to handle colour points
+points = [deque(maxlen=1024)]
+index = 0
 
 # The kernel to be used for dilation purpose
-kernel = np.ones((5,5),np.uint8)
+kernel = np.ones((5, 5), np.uint8)
 
-colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (0, 255, 255)]
-colorIndex = 0
+# Setting color to black
+color = (0, 0, 0)
+
+# Load the model and scaler
+model = load('../models/digit_recognizer')
+scaler = load('../models/scaler.joblib')
+
+# Function to predict digit
+def prediction(image, model, scaler):
+    img = cv2.resize(image, (28, 28))
+    img = img.flatten().reshape(1, -1)
+    img = scaler.transform(img)
+    predict = model.predict(img)
+    return predict[0]
 
 # Here is code for Canvas setup
-paintWindow = np.zeros((471,636,3)) + 255
-paintWindow = cv2.rectangle(paintWindow, (40,1), (140,65), (0,0,0), 2)
-paintWindow = cv2.rectangle(paintWindow, (160,1), (255,65), (255,0,0), 2)
-paintWindow = cv2.rectangle(paintWindow, (275,1), (370,65), (0,255,0), 2)
-paintWindow = cv2.rectangle(paintWindow, (390,1), (485,65), (0,0,255), 2)
-paintWindow = cv2.rectangle(paintWindow, (505,1), (600,65), (0,255,255), 2)
-
+paintWindow = np.zeros((471, 636, 3)) + 255
+paintWindow = cv2.rectangle(paintWindow, (40, 1), (140, 65), (0, 0, 0), 2)
 cv2.putText(paintWindow, "CLEAR", (49, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
-cv2.putText(paintWindow, "BLUE", (185, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
-cv2.putText(paintWindow, "GREEN", (298, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
-cv2.putText(paintWindow, "RED", (420, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
-cv2.putText(paintWindow, "YELLOW", (520, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
 cv2.namedWindow('Paint', cv2.WINDOW_AUTOSIZE)
 
 # initialize mediapipe
@@ -46,22 +42,10 @@ mpDraw = mp.solutions.drawing_utils
 
 # Initialize the webcam
 cap = cv2.VideoCapture(0)
-
-# Cargar el modelo y el escalador
-model = load('../models/digit_recognizer')
-scaler = load('../models/scaler.joblib')
-
-def prediction(image, model, scaler):
-    img = cv2.resize(image, (28, 28))
-    img = img.flatten().reshape(1, -1)
-    img = scaler.transform(img)
-    predict = model.predict(img)
-    return predict[0]
-
-while True:
+ret = True
+while ret:
+    # Read each frame from the webcam
     ret, frame = cap.read()
-    if not ret:
-        break
 
     x, y, c = frame.shape
 
@@ -69,16 +53,8 @@ while True:
     frame = cv2.flip(frame, 1)
     framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    frame = cv2.rectangle(frame, (40,1), (140,65), (0,0,0), 2)
-    frame = cv2.rectangle(frame, (160,1), (255,65), (255,0,0), 2)
-    frame = cv2.rectangle(frame, (275,1), (370,65), (0,255,0), 2)
-    frame = cv2.rectangle(frame, (390,1), (485,65), (0,0,255), 2)
-    frame = cv2.rectangle(frame, (505,1), (600,65), (0,255,255), 2)
+    frame = cv2.rectangle(frame, (40, 1), (140, 65), (0, 0, 0), 2)
     cv2.putText(frame, "CLEAR", (49, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
-    cv2.putText(frame, "BLUE", (185, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
-    cv2.putText(frame, "GREEN", (298, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
-    cv2.putText(frame, "RED", (420, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
-    cv2.putText(frame, "YELLOW", (520, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
 
     # Get hand landmark prediction
     result = hands.process(framergb)
@@ -90,89 +66,55 @@ while True:
             for lm in handslms.landmark:
                 lmx = int(lm.x * 640)
                 lmy = int(lm.y * 480)
+
                 landmarks.append([lmx, lmy])
 
             # Drawing landmarks on frames
             mpDraw.draw_landmarks(frame, handslms, mpHands.HAND_CONNECTIONS)
-        
-        fore_finger = (landmarks[8][0],landmarks[8][1])
+        fore_finger = (landmarks[8][0], landmarks[8][1])
         center = fore_finger
-        thumb = (landmarks[4][0],landmarks[4][1])
-        cv2.circle(frame, center, 3, (0,255,0),-1)
-        
-        if (thumb[1]-center[1]<30):
-            bpoints.append(deque(maxlen=512))
-            blue_index += 1
-            gpoints.append(deque(maxlen=512))
-            green_index += 1
-            rpoints.append(deque(maxlen=512))
-            red_index += 1
-            ypoints.append(deque(maxlen=512))
-            yellow_index += 1
+        thumb = (landmarks[4][0], landmarks[4][1])
+        cv2.circle(frame, center, 3, (0, 255, 0), -1)
 
+        if (thumb[1] - center[1] < 30):
+            points.append(deque(maxlen=512))
+            index += 1
         elif center[1] <= 65:
-            if 40 <= center[0] <= 140: # Clear Button
-                bpoints = [deque(maxlen=512)]
-                gpoints = [deque(maxlen=512)]
-                rpoints = [deque(maxlen=512)]
-                ypoints = [deque(maxlen=512)]
-
-                blue_index = 0
-                green_index = 0
-                red_index = 0
-                yellow_index = 0
-
-                paintWindow[67:,:,:] = 255
-            elif 160 <= center[0] <= 255:
-                    colorIndex = 0 # Blue
-            elif 275 <= center[0] <= 370:
-                    colorIndex = 1 # Green
-            elif 390 <= center[0] <= 485:
-                    colorIndex = 2 # Red
-            elif 505 <= center[0] <= 600:
-                    colorIndex = 3 # Yellow
+            if 40 <= center[0] <= 140:  # Clear Button
+                points = [deque(maxlen=512)]
+                index = 0
+                paintWindow[67:, :, :] = 255
         else:
-            if colorIndex == 0:
-                bpoints[blue_index].appendleft(center)
-            elif colorIndex == 1:
-                gpoints[green_index].appendleft(center)
-            elif colorIndex == 2:
-                rpoints[red_index].appendleft(center)
-            elif colorIndex == 3:
-                ypoints[yellow_index].appendleft(center)
+            points[index].appendleft(center)
     else:
-        bpoints.append(deque(maxlen=512))
-        blue_index += 1
-        gpoints.append(deque(maxlen=512))
-        green_index += 1
-        rpoints.append(deque(maxlen=512))
-        red_index += 1
-        ypoints.append(deque(maxlen=512))
-        yellow_index += 1
+        points.append(deque(maxlen=512))
+        index += 1
 
-    # Draw lines of all the colors on the canvas and frame
-    points = [bpoints, gpoints, rpoints, ypoints]
-    for i in range(len(points)):
-        for j in range(len(points[i])):
-            for k in range(1, len(points[i][j])):
-                if points[i][j][k - 1] is None or points[i][j][k] is None:
-                    continue
-                cv2.line(frame, points[i][j][k - 1], points[i][j][k], colors[i], 2)
-                cv2.line(paintWindow, points[i][j][k - 1], points[i][j][k], colors[i], 2)
+    # Draw lines on the canvas and frame
+    for j in range(len(points)):
+        for k in range(1, len(points[j])):
+            if points[j][k - 1] is None or points[j][k] is None:
+                continue
+            cv2.line(frame, points[j][k - 1], points[j][k], color, 2)
+            cv2.line(paintWindow, points[j][k - 1], points[j][k], color, 2)
 
-    # Crop the painting area and recognize digits
-    bbox = [(40, 65), (600, 471)]
-    img_cropped = paintWindow[bbox[0][1]:bbox[1][1], bbox[0][0]:bbox[1][0]]
-    img_gray = cv2.cvtColor(img_cropped, cv2.COLOR_BGR2GRAY)
-    _, thresh = cv2.threshold(img_gray, 128, 255, cv2.THRESH_BINARY_INV)
-    
-    if np.count_nonzero(thresh) > 100:  # Only predict if there are enough non-zero pixels
+    # Predict digit from the drawn image
+    if index > 0 and len(points[index - 1]) > 1:
+        bbox_size = (100, 100)
+        bbox = [(int(x // 2 - bbox_size[0] // 2), int(y // 2 - bbox_size[1] // 2)),
+                (int(x // 2 + bbox_size[0] // 2), int(y // 2 + bbox_size[1] // 2))]
+
+        img_cropped = paintWindow[bbox[0][1]:bbox[1][1], bbox[0][0]:bbox[1][0]]
+        img_gray = cv2.cvtColor(img_cropped, cv2.COLOR_BGR2GRAY)
+        _, thresh = cv2.threshold(img_gray, 128, 255, cv2.THRESH_BINARY_INV)
+
         digit = prediction(thresh, model, scaler)
-        cv2.putText(frame, f'Digit: {digit}', (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+
+        # Add digit prediction to frame
+        cv2.putText(frame, f'Digit: {digit}', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
     
     cv2.imshow("Output", frame)
     cv2.imshow("Paint", paintWindow)
-    cv2.imshow("Digit", thresh)
 
     if cv2.waitKey(1) == ord('q'):
         break
